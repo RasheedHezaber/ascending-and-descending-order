394592726b88c8bd4bc6fce46d54ec2f8724dc7f
RasheedHezaber <rasheedhezaber@gmail.com>
2020-09-05 22:38:34 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,7 @@
+fun main(){
+
+    val numbers = listOf(8, 4,7,1,2,3,0,5,6)
+
+    println("Sorted ascending: ${numbers.sorted()}")
+    println("Sorted descending: ${numbers.sortedDescending()}")
+}
